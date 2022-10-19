5b598b22448663b62d62af610eb6f04993f8b73e
Victorinox2 <uwaomaviktor@yahoo.com>
2022-10-19 15:05:37 -0700

    hey wu

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,38 @@
 #ifndef MAIN_N
-#define MAIN_H
+#define MAIN_N
 
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n):
 int _putchar(char c);
 
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int n);
+
+int print_last_digit(int n);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int x, int y);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+int main(void);
+
+int main(void);
+
+int main(void);
+
+int main(void);
 
 #endif
