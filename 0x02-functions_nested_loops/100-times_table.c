2fae076753b18b941ae3cca0a4ad4b7da97886fe
Victorinox2 <uwaomaviktor@yahoo.com>
2022-10-19 19:38:27 -0700

    i am not weak

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,101 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * print_times_table - prints times table
+ *
+ *   * @n : times table to use
+ *
+ *    * Return:void
+ *
+ *     */
+
+
+
+void print_times_table(int n)
+
+{
+
+
+
+	int a = 0, prodct, b;
+
+
+
+	if (n < 0 || n > 15)
+
+			return;
+
+
+
+	while (a <= n)
+
+	{
+
+			for (b = 0; b <= n; b++)
+
+					{
+
+								prodct = a * b;
+
+										if (b == 0)
+
+														_putchar('0' + prodct);
+
+												else if (prodct < 10)
+
+															{
+
+																			_putchar(' ');
+
+																						_putchar(' ');
+
+																									_putchar('0' + prodct);
+
+																											}
+
+														else if (prodct < 100)
+
+																	{
+
+																					_putchar(' ');
+
+																								_putchar('0' + prodct / 10);
+
+																											_putchar('0' + prodct % 10);
+
+																													}
+
+																else
+
+																			{
+
+																							_putchar('0' + prodct / 100);
+
+																										_putchar('0' + (prodct - 100) / 10);
+
+																													_putchar('0' + prodct % 10);
+
+																															}
+
+																		if (b < n)
+
+																					{
+
+																									_putchar(',');
+
+																												_putchar(' ');
+
+																														}
+
+																			}
+
+				_putchar('\n');
+
+					a++;
+
+	}
+
+}
