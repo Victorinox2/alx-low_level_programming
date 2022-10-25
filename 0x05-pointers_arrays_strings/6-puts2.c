9a09d277cc1cde19bc2f05d0751efb7257abac03
Victorinox2 <uwaomaviktor@yahoo.com>
2022-10-24 19:17:19 -0700

    i will not lose

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,43 @@
+nclude "main.h"
+
+
+
+/**
+ *
+ *  * puts2 - prints a string followed by a new line
+ *
+ *   * @str: pointer tostring to be printed
+ *
+ *    * Return: void
+ *
+ *     */
+
+
+
+void puts2(char *str)
+
+{
+
+		int i = 0;
+
+
+
+			while (str[i] != '\0')
+
+					{
+
+								if (i % 2 == 0)
+
+											{
+
+															_putchar(str[i]);
+
+																	}
+
+										i++;
+
+											}
+
+				_putchar('\n');
+
+}
