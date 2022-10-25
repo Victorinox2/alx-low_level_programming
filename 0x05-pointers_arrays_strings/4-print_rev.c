d4c596fd9a8c3ac0cc1d16ea16e908477e5c050e
Victorinox2 <uwaomaviktor@yahoo.com>
2022-10-24 19:02:30 -0700

    roc

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * print_rev- prints reversed string, followed by a new line
+ *
+ *   * @s: pointer to the string to be printed in reverse
+ *
+ *    * Return: void
+ *
+ *     */
+
+
+
+void print_rev(char *s)
+
+{
+
+		int i = 0;
+
+
+
+			while (s[i])
+
+						i++;
+
+
+
+				while (i--)
+
+						{
+
+									_putchar(s[i]);
+
+										}
+
+
+
+					_putchar('\n');
+
+}
