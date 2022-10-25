3549125230e337f5b766422d608bd198b723aa38
Victorinox2 <uwaomaviktor@yahoo.com>
2022-10-24 18:44:59 -0700

    chrome meets chrome

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * _strlen - find the length of a string of characters
+ *
+ *   * @s: pointer to the string
+ *
+ *    * Return: void
+ *
+ *     */
+
+
+
+int _strlen(char *s)
+
+{
+
+		int i = 0;
+
+
+
+			while (s[i])
+
+						i++;
+
+
+
+				return (i);
+
+}
