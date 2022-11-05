8696ca53277315d4cd9f50d031ad7165a3241d0a
Victorinox2 <uwaomaviktor@yahoo.com>
2022-11-04 20:24:10 -0700

    strong

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,25 +1,29 @@
-nclude <stdio.h>
+#include "stdio.h"
 
 
 
 /**
  *
- * * main -> this is a function to print its name
+ *  * main - prints program name, followed by a new line
  *
- * * @argc: argc parameter
+ *   * @argc: int
  *
- * * @argv: an array of a command listed
+ *    * @argv: list
  *
- * * Return: 0 for success
+ *     * Return: 0
  *
- * */
+ *      */
 
-int main(int argc __attribute__((unused)), char *argv[])
+
+
+int main(int argc, char *argv[])
 
 {
 
-		printf("%s\n", *argv);
+		(void)argc;
 
-			return (0);
+			printf("%s\n", argv[0]);
+
+				return (0);
 
 }
