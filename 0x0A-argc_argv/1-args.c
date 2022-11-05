edcbf1c30a92fa1ee1d49c0556ba24281f863745
Victorinox2 <uwaomaviktor@yahoo.com>
2022-11-04 20:27:54 -0700

    practice makes perfect

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,23 @@
+nclude "stdio.h"
+
+/**
+ *
+ *  * main - prints number of arguments, followed by a new line
+ *
+ *   * @argc: int
+ *
+ *    * @argv: list
+ *
+ *     * Return: 0
+ *
+ *      */
+
+int main(int argc, char *argv[]__attribute__((unused)))
+
+{
+
+		printf("%d\n", argc - 1);
+
+			return (0);
+
+}
