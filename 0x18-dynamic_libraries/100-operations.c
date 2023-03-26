4d65724cde8068c24de5b0c26d5231ed448a60b4
Victorinox2 <uwaomaviktor@gmail.com>
2023-03-25 20:17:59 -0700

    Dynamic

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * add -adds two integers and returns the result
+ *@a: int to be added to b
+ *@b: int to be added to a
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * mul - returns the product of its parameters
+ *@a: int type number
+ *@b: int type number
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * sub - returns the difference between its parameters
+ *@a: int type number
+ *@b: int type number
+ * Return: 0
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * div - returns the fraction of one parameter
+ *@a: int type number
+ *@b: int type number
+ * Return: 0
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - returns the remainder after division of its parameters
+ *@a: int type number
+ *@b: int type number
+ * Return: 0
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
