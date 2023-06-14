136615e9424136595906118ed859b130cae837cc
Victorinox2 <uwaomaviktor@yahoo.com>
2023-06-14 14:08:13 -0700

    Memory Allocation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,35 +1,25 @@
 #include "main.h"
 #include <stdlib.h>
-		
 /**
  * File: 0-create_array.c
- * Author; Victor Uwaoma		
+ * Author: Victor Uwaoma
  * create_array - create an array of size size and assign char c
-		
  * @size: size of array
-		
  * @c: char to assign
-		
- * Description: creat an array of size size and assign char c
-		
- * Return: pointer to array, NULL if fail
+ * Description: create an array of size size and assign char c
+ * Return: pointer to an array, NULL if fail
+ *
  */
- 
-		
 char *create_array(unsigned int size, char c)
 {
-	char *s = malloc(size * sizeof(char));
-	unsigned int i = 0;
+	char *str;
+	unsigned int i;
 
-	if (size == 0 || s == 0)
-	{
-		return ('\0');
-	}
-	while (i < size)
-	{
-		s[i] = c;
-		i++;
-	}
-	return (s);
-	free(s);
+	str = malloc(sizeof(char) * size);
+	if (size == 0 || str == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		str[i] = c;
+	return (str);
 }
