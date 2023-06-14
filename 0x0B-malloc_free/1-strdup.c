6340b8ca182fe711ec88d6dae71d64bf5f61bea5
Victorinox2 <uwaomaviktor@yahoo.com>
2023-06-14 14:17:15 -0700

    Memory Allocation

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,12 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+ * File: 0-create_array.c
+ * Author: Victor Uwaoma
  * create_array - create array of size size and assign char c
- * @size: size of array
+ * @size: size of the array
  * @c: char to assign
- * Description: create array of size size and assign char c
+ * Description: create an array of size size and assign char c
  * Return: pointer to array, NULL if fail
- *
  */
 char *create_array(unsigned int size, char c)
 {
